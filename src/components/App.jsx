df05b8dd244050fad1aa770ba0964474b50ec2e5
Genokrad <zeeper90@gmail.com>
2023-01-22 01:57:30 +0200

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,37 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+import { Section } from './Section/Section';
+import { Form } from './Form/Form';
+import { Search } from './Search/Search';
+import { Contacts } from './Contacts/Contacts';
 
 export class App extends Component {
   state = {
-    // contacts: [],
-    // name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleChange = event => {
-    console.log(event.target.value);
+  sendData = data => {
+    const { contacts } = this.state;
+    this.setState({ contacts: [...contacts, data] });
+    console.log(this.sate);
   };
 
   render() {
-    // const { contacts, name } = this.state;
     return (
-      <form>
-        <p>Name</p>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={this.handleChange}
-        />
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={this.handleChange}
-        />
-        <button>Add contact</button>
-      </form>
+      <>
+        <Section title="Phonebook">
+          <Form send={this.sendData} />
+        </Section>
+        <Section title="Contacts">
+          <Search />
+          <Contacts />
+        </Section>
+      </>
     );
   }
 }
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,8 @@
+export const Section = ({ title, children }) => {
+  return (
+    <div>
+      <p>{title}</p>
+      {children}
+    </div>
+  );
+};
